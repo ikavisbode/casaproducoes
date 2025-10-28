fa617a23d2c2a6f68436f9d995da393c3cd54ce7
ikavisbode <89670775+ikavisbode@users.noreply.github.com>
2025-10-28 02:50:18 -0300

    changed text

--- components/featured.tsx
+++ components/featured.tsx
@@ -13,10 +13,9 @@ export default function Featured() {
         />
       </div>
       <div className="flex-1 text-left lg:h-[800px] flex flex-col justify-center lg:mr-12 lg:order-1">
-        <h3 className="uppercase mb-4">Features That Refuse to Sit Still</h3>
+        <h3 className="uppercase mb-4">Nossos Filmes</h3>
         <p className="text-2xl lg:text-4xl mb-8">
-          Not just bullet points—living, breathing highlights. Each feature adapts to movement, context, and mood,
-          making your product feel alive from the very first glance.
+          Cada um feito com toda a dedicação da nossa equipe, independente de orçamentos ou limitações. Todos feitos com muita atenção.
         </p>
         <button className="bg-black text-white border border-black px-4 py-2 text-sm transition-all duration-300 hover:bg-white hover:text-black cursor-pointer w-fit">
           LEARN MORE
--- components/hero.tsx
+++ components/hero.tsx
@@ -25,10 +25,9 @@ export default function Hero() {
         />
         <div className="absolute inset-0 flex items-center justify-start z-10">
           <div className="text-left text-white max-w-3xl px-6">
-            <h1 className="text-3xl md:text-5xl font-bold mb-6 leading-tight">Where Ideas Learn to Breathe</h1>
+            <h1 className="text-3xl md:text-5xl font-bold mb-6 leading-tight">Casa Produções</h1>
             <p className="text-sm md:text-base leading-relaxed mb-8">
-              A landing space for bold experiments, half-finished thoughts, and sparks of inspiration. Built to grow,
-              adapt, and surprise—just like the projects you're about to launch.
+              A Produtora nasce com o objetivo de viabilizar projetos de diferentes portes. Acreditamos em filmes. Abraçamos todas as linguagens e nossa vontade é dar fruição as ideias.
             </p>
             <button className="px-4 py-2 border-2 border-white bg-transparent text-white text-sm transition-all duration-300 hover:bg-white hover:text-black cursor-pointer">
               GET STARTED
